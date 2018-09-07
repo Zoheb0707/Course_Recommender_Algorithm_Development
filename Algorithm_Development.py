6bc73ea293955128540b48058ee8cf5bc299a757
Zoheb0707 <zhs142013@yahoo.in>
2018-09-07 11:57:28 -0700

    almost done predicting for new student

--- Algorithm_Development.py
+++ Algorithm_Development.py
@@ -8,7 +8,8 @@ Created on Sun Sep  2 18:29:52 2018
 import requests
 import pandas as pd
 
-student_id_list = [] #get list of student ids from the database I think
+student_id_list = [1582283,1402365,1386886,1407986,1493884,1482382,1578352,1116735,1484448,
+                   1337211,1475699,1727787,1378711,1620340,1398754]
 
 user_student_id = '' #get student id from user
 class_type_needed = '' #get class type from user
@@ -39,13 +40,21 @@ def create_dataframe(student_ids):
                                  str(student_id)).json()
         major_list = [json_data['Major1'], json_data['Major2'], json_data['Major3']]
         course_list = list(set(json_data['courses']))
-        data_list.append(student_id, major_list, course_list)
+        data_list.append([student_id, major_list, course_list])
     return pd.DataFrame(data=data_list, columns=columns)
 
 student_data_frame = create_dataframe(student_id_list)
 
+major_priority_dict = {user_major_list[1]:3, user_major_list[2]:2, user_major_list[3]:1}
+
 #prediction for new user
 if new_student:
-    student_data_frame = student_data_frame[set(student_data_frame['major_list']).
-                                            intersection(set(user_major_list)) == True]
+    elements_to_keep = []
+    for i in range(0, len(student_data_frame)):
+        if len(set(student_data_frame.iloc[i]['major_list']).intersects(set(user_major_list))) > 0:
+            elements_to_keep.append(i)
+    student_data_frame = student_data_frame.iloc[elements_to_keep]
+    course_popularity = {}
+    
+    
     
